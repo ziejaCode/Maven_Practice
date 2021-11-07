91049998bee4054714f0ee51578b7c94c33b3911
ziejaCode <49070294+ziejaCode@users.noreply.github.com>
2021-11-07 18:09:42 +0000

    Initial commit

---
+++ mavenStartup/src/main/java/com/ziejacode/mavenStartup/App.java
@@ -0,0 +1,13 @@
+package com.ziejacode.mavenStartup;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
